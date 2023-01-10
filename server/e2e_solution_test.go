b01c61d1a47b23a0b0d6b630a7f155966f5e424b
Todd Beets <todd@beetsme.net>
2023-01-10 08:52:10 -0800

    note leaf membrane sub interest issues

--- server/e2e_solution_test.go
+++ server/e2e_solution_test.go
@@ -30,43 +30,43 @@ func startStore(t *testing.T, serverConfig []byte, hubPort int, hubUser string, 
 
 func TestE2ERetailSolutionRetailFleetBus(t *testing.T) {
 	/*
-			E2E configuration for a retail cloud application accounts interacting bi-directionally with a fleet of
-			retail stores.  Each retail store has multiple store application accounts.  Tests the topology of a hub with
-			multiple leafs (cloud with multiple edges) where the leafs are inherently "untrusted" by the hub (i.e. hub-resident
-			controls must protect against "rogue" stores).
+		E2E configuration for a retail cloud application accounts interacting bi-directionally with a fleet of
+		retail stores.  Each retail store has multiple store application accounts.  Tests the topology of a hub with
+		multiple leafs (cloud with multiple edges) where the leafs are inherently "untrusted" by the hub (i.e. hub-resident
+		controls must protect against "rogue" stores).
 
-			Store NATS configuration has a unique RETAIL cloud credential (with suitable permissions).
-		    Store NATS configuration needs to know the store id of its instance.
-			Store NATS configuration imports messages from each store app by unique app name.
-			Store does not need to know cloud app names.
-			Store apps have a unique app name.
-			Store apps do not need to know the store id they are executing in.
+		Store NATS configuration has a unique RETAIL cloud credential (with suitable permissions).
+		Store NATS configuration needs to know the store id of its instance.
+		Store NATS configuration imports messages from each store app by unique app name.
+		Store does not need to know cloud app names.
+		Store apps have a unique app name.
+		Store apps do not need to know the store id they are executing in.
 
-			Cloud NATS configuration imports messages from each cloud app by unique app name.
-			Cloud apps have a unique app name.
-			Cloud does not need to know store app names.
+		Cloud NATS configuration imports messages from each cloud app by unique app name.
+		Cloud apps have a unique app name.
+		Cloud does not need to know store app names.
 
-			A cloud app publishes as: bus.retail.{app name}.{class}.{store id}.>
-				bus.retail.{app name}.{class}.2.> to publish messages that will be received by store id 2 only
-				bus.retail.{app name}.{class}.ALL.> to publish messages that will be received by all stores
-				bus.retail.{app name}.{class}.NONE.> to publish messages that will be received by no stores (only cloud apps)
+		A cloud app publishes as: bus.retail.{app name}.{class}.{store id}.>
+			bus.retail.{app name}.{class}.2.> to publish messages that will be received by store id 2 only
+			bus.retail.{app name}.{class}.ALL.> to publish messages that will be received by all stores
+			bus.retail.{app name}.{class}.NONE.> to publish messages that will be received by no stores (only cloud apps)
 
-			A cloud app subscribes as: bus.retail.{app name}.{class}.{store id}.>
-				bus.retail.> to see all publishes from both cloud and store
-				bus.retail.CLOUD-APP-2.> to see all publishes from cloud app CLOUD-APP-2 only
-				bus.retail.STORE.> to see all publishes from stores only
-				bus.retail.STORE.*.2.> to see publishes just from store id 2)
+		A cloud app subscribes as: bus.retail.{app name}.{class}.{store id}.>
+			bus.retail.> to see all publishes from both cloud and store
+			bus.retail.CLOUD-APP-2.> to see all publishes from cloud app CLOUD-APP-2 only
+			bus.retail.STORE.> to see all publishes from stores only
+			bus.retail.STORE.*.2.> to see publishes just from store id 2)
 
-			A store app publishes as: bus.store.{app name}.{class}.>
+		A store app publishes as: bus.store.{app name}.{class}.>
 
-			A store app subscribes as: bus.store.{app name}.{class}.>
-				bus.store.> to see all publishes from apps in the same store and cloud app publishes shared with the store
-				bus.store.APP-3.> to see all publishes from a specific store app in the same store
-				bus.store.CLOUD.> to see all publishes just from cloud apps
+		A store app subscribes as: bus.store.{app name}.{class}.>
+			bus.store.> to see all publishes from apps in the same store and cloud app publishes shared with the store
+			bus.store.APP-3.> to see all publishes from a specific store app in the same store
+			bus.store.CLOUD.> to see all publishes just from cloud apps
 
-			Cloud apps and store apps can be configured to receive only specific classes (e.g. data classificaiton) of messages
-			through specific import specification.  See store app APP-2 import below where only C4 messages from cloud apps
-			and other same-store apps will be seen by APP-2.
+		Cloud apps and store apps can be configured to receive only specific classes (e.g. data classificaiton) of messages
+		through specific import specification.  See store app APP-2 import below where only C4 messages from cloud apps
+		and other same-store apps will be seen by APP-2.
 	*/
 	cloudConfig := []byte(`
 		port: %[1]s
@@ -83,13 +83,19 @@ func TestE2ERetailSolutionRetailFleetBus(t *testing.T) {
 					{ 
 						user: "user-store-1", password: "s3cr3t"
 						permissions: {
-							# Solicitor-side (leaf-side is the solicitor, hub-side is the solicited) sees reversed publish/subscribe perms
+							# Leaf Connection
+							# Solicitor-side (leaf-side is the solicitor, hub-side is the solicited) applies 
+							# pub/sub perms reversed.
+							#
+							# Both solicited and solicitor transfer any respective client subs to create cross-membrane
+							# subscription interest. The transferred subs are pruned by permission scope.
 
 							# Uplink
 							# Subscription for bus.retail.STORE.*.1.> passed to Leaf
 							# Leaf may publish bus.retail.STORE.*.1.> to Hub
 							# Subscription for bus.store.> is passed to the Hub
 							# Any rogue bus.store.> publishes to hub not exported from RETAIL
+							# Issue #1: rogue bus.retail.STORE.> subscription from any store
 
 							publish: [ "bus.store.>", "bus.retail.STORE.*.1.>" ]
 
@@ -98,6 +104,7 @@ func TestE2ERetailSolutionRetailFleetBus(t *testing.T) {
 							# Hub may publish bus.store.CLOUD.*.1.> and bus.store.CLOUD.*.ALL.> to Leaf
 							# Subscription for bus.retail.> passed to Leaf
 							# Any rogue bus.retail.> subscribes from hub not exported to RETAIL
+							# Issue #2: rogue bus.store.CLOUD.> publish down to any store 
 
 							subscribe: [ "bus.retail.>", "bus.store.CLOUD.*.1.>", "bus.store.CLOUD.*.ALL.>" ]
 						}
