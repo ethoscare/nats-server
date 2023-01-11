4cd1093f7943db02bc1fde9312cf0e9ac754f07a
Todd Beets <todd@beetsme.net>
2023-01-10 22:44:15 -0800

    more tests but also some debugging aids for leaf subz propagation

--- server/e2e_solution_test.go
+++ server/e2e_solution_test.go
@@ -2,30 +2,84 @@ package server
 
 import (
 	"fmt"
+	"github.com/nats-io/nats.go"
 	"net/url"
 	"strconv"
 	"testing"
 	"time"
 )
 
-func startCloud(t *testing.T, serverConfig []byte, cloudPort int) (*Server, string) {
-	configStr := fmt.Sprintf(string(serverConfig), strconv.Itoa(cloudPort))
+func startRetailCloud(t *testing.T, serverConfig []byte, cloudPort int, serverName string, httpPort int) (*Server, string) {
+	configStr := fmt.Sprintf(string(serverConfig), strconv.Itoa(cloudPort), serverName, strconv.Itoa(httpPort))
 	cloudConfig := createConfFile(t, []byte(configStr))
 	cloudServer, _ := RunServerWithConfig(cloudConfig)
 	return cloudServer, cloudConfig
 }
 
-func startStore(t *testing.T, serverConfig []byte, hubPort int, hubUser string, storeId string, storePort int) (*Server, string) {
+func startRetailStore(t *testing.T, serverConfig []byte, hubPort int, hubUser string, storeId string, storePort int, serverName string, httpPort int) (*Server, string) {
 	u, err := url.Parse(fmt.Sprintf("nats-leaf://%s:s3cr3t@localhost:%d", hubUser, hubPort))
 	if err != nil {
 		t.Fatalf("Error parsing url: %v", err)
 	}
-	configStr := fmt.Sprintf(string(serverConfig), u.String(), storeId, strconv.Itoa(storePort))
+	configStr := fmt.Sprintf(string(serverConfig), u.String(), storeId, strconv.Itoa(storePort), serverName, strconv.Itoa(httpPort))
 	storeConfig := createConfFile(t, []byte(configStr))
 	storeServer, _ := RunServerWithConfig(storeConfig)
 	time.Sleep(1 * time.Second)
 	checkLeafNodeConnectedCount(t, storeServer, 1)
 	return storeServer, storeConfig
+}
+
+func createRetailClient(t *testing.T, s *Server, u string) *nats.Conn {
+	// Create test clients
+	c, err := nats.Connect(fmt.Sprintf("%s:%s@%s", u, "s3cr3t", s.getClientConnectURLs()[0]))
+	require_True(t, err == nil)
+	require_True(t, c != nil)
+	return c
+}
+
+func createRetailFleetBusClients(t *testing.T, cloud *Server, store1 *Server, store2 *Server) map[string]*nats.Conn {
+	connMap := make(map[string]*nats.Conn)
+	cloudUsers := []string{"user-retail-admin", "user-cloud-app-1", "user-cloud-app-2", "user-cloud-app-3"}
+	storeUsers := []string{"user-store-admin", "user-app-1", "user-app-2", "user-app-3"}
+
+	if cloud != nil {
+		for _, user := range cloudUsers {
+			connMap[user] = createRetailClient(t, cloud, user)
+		}
+	}
+	if store1 != nil {
+		for _, user := range storeUsers {
+			connMap["store1-"+user] = createRetailClient(t, store1, user)
+		}
+	}
+	if store2 != nil {
+		for _, user := range storeUsers {
+			connMap["store2-"+user] = createRetailClient(t, store2, user)
+		}
+	}
+	return connMap
+}
+
+func closeRetailFleetBusClients(t *testing.T, cs map[string]*nats.Conn) {
+	t.Helper()
+	for _, c := range cs {
+		c.Close()
+	}
+}
+
+func resetRetailClientSub(t *testing.T, sub *nats.Subscription) {
+	t.Helper()
+	var err error
+	for err == nil {
+		_, err = sub.NextMsg(2 * time.Millisecond)
+	}
+}
+
+func resetRetailClientSubSet(t *testing.T, subs map[string]*nats.Subscription) {
+	t.Helper()
+	for _, sub := range subs {
+		resetRetailClientSub(t, sub)
+	}
 }
 
 func TestE2ERetailSolutionRetailFleetBus(t *testing.T) {
@@ -68,8 +122,13 @@ func TestE2ERetailSolutionRetailFleetBus(t *testing.T) {
 		through specific import specification.  See store app APP-2 import below where only C4 messages from cloud apps
 		and other same-store apps will be seen by APP-2.
 	*/
+
+	var err error
+
 	cloudConfig := []byte(`
 		port: %[1]s
+		server_name: %[2]s
+		http_port: %[3]s
 		leafnodes: {
 			listen: "127.0.0.1:-1"
 		}
@@ -171,6 +230,8 @@ func TestE2ERetailSolutionRetailFleetBus(t *testing.T) {
 
 	storeConfig := []byte(`
 		port: %[3]s
+		server_name: %[4]s
+		http_port: %[5]s
 		leaf {
 			# RETAIL (hub) <-> STORE (leaf)
 			remotes [
@@ -237,49 +298,139 @@ func TestE2ERetailSolutionRetailFleetBus(t *testing.T) {
 		system_account: "SYS"
 	`)
 
-	cloud, cloudConfigFile := startCloud(t, cloudConfig, -1)
+	cloud, cloudConfigFile := startRetailCloud(t, cloudConfig, 4222, "cloud", 8222)
 	require_True(t, cloud != nil)
 	require_True(t, cloudConfigFile != "")
 	defer cloud.Shutdown()
 	defer removeFile(t, cloudConfigFile)
 
-	store1, storeConfigFile1 := startStore(t, storeConfig, cloud.leafNodeInfo.Port, "user-store-1", "1", -1)
+	store1, storeConfigFile1 := startRetailStore(t, storeConfig, cloud.leafNodeInfo.Port, "user-store-1", "1", 4322, "store1", 8322)
 	require_True(t, store1 != nil)
 	require_True(t, storeConfigFile1 != "")
 	defer store1.Shutdown()
 	defer removeFile(t, storeConfigFile1)
 
-	store2, storeConfigFile2 := startStore(t, storeConfig, cloud.leafNodeInfo.Port, "user-store-2", "2", -1)
+	store2, storeConfigFile2 := startRetailStore(t, storeConfig, cloud.leafNodeInfo.Port, "user-store-2", "2", 4422, "store2", 8422)
 	require_True(t, store2 != nil)
 	require_True(t, storeConfigFile2 != "")
 	defer store2.Shutdown()
 	defer removeFile(t, storeConfigFile2)
 
-	// Uncomment and change cloudPort and storePort ports to fixed numbers (e.g. 4222, 4322, 4422) for quick ad-hoc environment
-	//fmt.Printf("cloud url: [%s]\n", cloud.clientConnectURLs[0])
-	//fmt.Printf("store1 url: [%s]\n", store1.clientConnectURLs[0])
-	//fmt.Printf("store2 url: [%s]\n", store2.clientConnectURLs[0])
-	//select {}
+	// Create test clients and subs
+	c := createRetailFleetBusClients(t, cloud, store1, store2)
+	defer closeRetailFleetBusClients(t, c)
 
-	// Cloud app pub to specific store
-	// nats --server localhost:4222 --user user-cloud-app-1 --password s3cr3t pub bus.retail.CLOUD-APP-1.C4.2.order.blah.blah "hello from cloud-app-1 {{.TimeStamp}}"
-	// Other cloud apps should see, only store 2 should see
+	subs1 := make(map[string]*nats.Subscription)
+	subs1["user-cloud-app-1"], err = c["user-cloud-app-1"].SubscribeSync("bus.retail.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["user-cloud-app-2"], err = c["user-cloud-app-2"].SubscribeSync("bus.retail.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["user-cloud-app-3"], err = c["user-cloud-app-3"].SubscribeSync("bus.retail.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["store1-user-app-1"], err = c["store1-user-app-1"].SubscribeSync("bus.store.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["store1-user-app-2"], err = c["store1-user-app-2"].SubscribeSync("bus.store.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["store1-user-app-3"], err = c["store1-user-app-3"].SubscribeSync("bus.store.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["store2-user-app-1"], err = c["store2-user-app-1"].SubscribeSync("bus.store.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["store2-user-app-2"], err = c["store2-user-app-2"].SubscribeSync("bus.store.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	subs1["store2-user-app-3"], err = c["store2-user-app-3"].SubscribeSync("bus.store.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+
+	// Time for store subcription interest to propogate up to cloud (for downlink) and down to store (for uplink)
+	time.Sleep(100 * time.Millisecond)
+
+	// Cloud app pub to specific store (#1)
+	err = c["user-cloud-app-1"].Publish("bus.retail.CLOUD-APP-1.C4.1.order.blah.blah", []byte("from cloud-app-1 to store 1"))
+	if err != nil {
+		t.Fatalf("expected to be able to publish: %v", err)
+	}
+	checkSubsPending(t, subs1["user-cloud-app-1"], 1)  // itself
+	checkSubsPending(t, subs1["user-cloud-app-2"], 1)  // another cloud app
+	checkSubsPending(t, subs1["store2-user-app-1"], 0) // a store 2 app
+	checkSubsPending(t, subs1["store1-user-app-1"], 1) // a store 1 app
+	resetRetailClientSubSet(t, subs1)
+
+	// Cloud app pub to specific store (#2)
+	err = c["user-cloud-app-1"].Publish("bus.retail.CLOUD-APP-1.C4.2.order.blah.blah", []byte("from cloud-app-1 to store 2"))
+	if err != nil {
+		t.Fatalf("expected to be able to publish: %v", err)
+	}
+	checkSubsPending(t, subs1["user-cloud-app-1"], 1)  // itself
+	checkSubsPending(t, subs1["user-cloud-app-2"], 1)  // another cloud app
+	checkSubsPending(t, subs1["store2-user-app-1"], 1) // a store 2 app
+	checkSubsPending(t, subs1["store1-user-app-1"], 0) // a store 1 app
+	resetRetailClientSubSet(t, subs1)
 
 	// Cloud app pub to all stores
-	// nats --server localhost:4222 --user user-cloud-app-1 --password s3cr3t pub bus.retail.CLOUD-APP-1.C4.ALL.order.blah.blah "hello from cloud-app-1 {{.TimeStamp}}"
-	// Other cloud apps should see, all stores should see
+	err = c["user-cloud-app-1"].Publish("bus.retail.CLOUD-APP-1.C4.ALL.order.blah.blah", []byte("from cloud-app-1 to all stores"))
+	if err != nil {
+		t.Fatalf("expected to be able to publish: %v", err)
+	}
+	checkSubsPending(t, subs1["store1-user-app-1"], 1) // a store 1 app
+	checkSubsPending(t, subs1["store2-user-app-1"], 1) // a store 2 app
+	resetRetailClientSubSet(t, subs1)
 
 	// Cloud app pub to other cloud apps only
-	// nats --server localhost:4222 --user user-cloud-app-1 --password s3cr3t pub bus.retail.CLOUD-APP-1.C4.NONE.order.blah.blah "hello from cloud-app-1 {{.TimeStamp}}"
-	// Other cloud apps should see, no stores should see
+	err = c["user-cloud-app-1"].Publish("bus.retail.CLOUD-APP-1.C4.NONE.order.blah.blah", []byte("from cloud-app-1 to no stores"))
+	if err != nil {
+		t.Fatalf("expected to be able to publish: %v", err)
+	}
+	checkSubsPending(t, subs1["user-cloud-app-2"], 1)  // a cloud app
+	checkSubsPending(t, subs1["store1-user-app-1"], 0) // a store 1 app
+	checkSubsPending(t, subs1["store2-user-app-1"], 0) // a store 2 app
+	resetRetailClientSubSet(t, subs1)
 
-	// Cloud app subs to all retail bus messages (cloud origin and store origin)
-	// nats --server localhost:4222 --user user-cloud-app-3 --password s3cr3t sub "bus.retail.>"
-	// Should see messages published from itself, another cloud-app, and a store
+	// Uncomment below and change cloudPort, storePort, and httpPort to fixed numbers (e.g. 4222/8222, 4322/8322, 4422/8422) for quick ad-hoc environment
+	fmt.Printf("ad-hoc testing")
+	select {}
 
-	// Cloud app subs to all retail bus messages from a specific cloud app
-	// nats --server localhost:4222 --user user-cloud-app-3 --password s3cr3t sub "bus.retail.CLOUD-APP-2.>"
-	// Should see messages published from CLOUD-APP-2 only
+	// Cloud app subs to all retail bus messages from a specific cloud app only
+	adhocSub, err := c["user-cloud-app-3"].SubscribeSync("bus.retail.CLOUD-APP-2.>")
+	if err != nil {
+		t.Fatalf("expected to be able to subscribe: %v", err)
+	}
+	err = c["user-cloud-app-2"].Publish("bus.retail.CLOUD-APP-2.C4.42.order.blah.blah", []byte("from cloud-app-2"))
+	if err != nil {
+		t.Fatalf("expected to be able to publish: %v", err)
+	}
+	err = c["user-cloud-app-1"].Publish("bus.retail.CLOUD-APP-1.C4.69.order.blah.blah", []byte("from cloud-app-1"))
+	if err != nil {
+		t.Fatalf("expected to be able to publish: %v", err)
+	}
+	checkSubsPending(t, adhocSub, 1)
+	adhocSub.Unsubscribe()
+
+	// Store app pub to cloud apps, other same-store apps, and itself
+	err = c["store1-user-app-1"].Publish("bus.store.APP-1.C4.order.blah.blah", []byte("from store1-user-app-1"))
+	if err != nil {
+		t.Fatalf("expected to be able to publish: %v", err)
+	}
+	checkSubsPending(t, subs1["store1-user-app-1"], 1) // itself
+	checkSubsPending(t, subs1["store1-user-app-2"], 1) // another same-store app
+	checkSubsPending(t, subs1["user-cloud-app-1"], 1)  // a cloud app
+	checkSubsPending(t, subs1["store2-user-app-1"], 0) // A different store app
 
 	// Cloud app subs to all retail bus messages from store apps only
 	// nats --server localhost:4222 --user user-cloud-app-3 --password s3cr3t sub "bus.retail.STORE.>"
